ce35cce9bb1d55538b9084dad5a32cbefeccd980
Joao Vitor <joao.vitor@JASSY.APOLLO>
2020-09-17 18:33:27 -0300

    General improvement on postProcessor

--- drophere.py
+++ drophere.py
@@ -3,15 +3,22 @@ import os
 import matplotlib.pyplot as plt
 import numpy
 
+##Simulation settings
+blender_frames =  2000
+blender_timestep = 0.01
+blender_groud_speed = 18 #m/s
+blender_dose = 100 #kg/ha
+blender_density = 1 #g/cm^3
+blender_simulation_time = blender_frames * blender_timestep
 
-def timeDistribution(T,S, discrete_time_step):
+def getVolumeDiferential(T,S,discrete_time_step, simulation_time_step, frames):
+    ###Converts dying times to volume increment for each timestep
+
     T_ = [i for i in T]
     S_ = [i for i in S]
-    time_step = 0.01
-    discrete_period = int(discrete_time_step / time_step)
-    sim_frames = 2000
-    accumulated_volume = []
-    for i in range(0,sim_frames, discrete_period):
+    discrete_period = int(discrete_time_step / simulation_time_step)
+    differential_volume = []
+    for i in range(0,frames, discrete_period):
         step_volume = 0
         eval_time = i
         for j in reversed(range(len(T_))):
@@ -19,42 +26,30 @@ def timeDistribution(T,S, discrete_time_step):
                 step_volume += (S_[j]/10)**3*3.1415
                 del S_[j]
                 del T_[j]
-        accumulated_volume.append(step_volume)
-    return accumulated_volume
+        differential_volume.append(step_volume)
+    return differential_volume
 
 
 
-def getDistribution(X,S,step_size):
-    x = [i for i in X]
-    s = [i for i in S]
-    steps = int((max(x)-min(x))/step_size)
-    startx = min(x)
-    diff_volume = []
-    for step in range(steps):
-        eval_x = startx + step * step_size
-        eval_volume = 0
-        for i in reversed(range(len(x))):
-            if x[i] <= eval_x:
-                eval_volume += s[i]**3
-                del x[i]
-                del s[i]
-        diff_volume.append(eval_volume)  
-    return diff_volume  
+def integrateVolumeDiferential(differential_volume):
+    volume = [sum(differential_volume[0:i]) for i in range(len(differential_volume))] 
+    return volume  
 
 
-
-def calc_cv(diff_volume, samples):
-    measurements = len(diff_volume)
-    cv = measurements * [None]
-    if measurements < samples:
+def calc_cv(differential_volume, samples):
+    differential_volume_steps = len(differential_volume)
+     
+    cv = differential_volume_steps * [None]
+    if differential_volume_steps < samples:
         print("Not enough samples for cv calculation... Exiting")
         input()
         quit()
         return cv 
+
     samples = int(samples/2)
-    for i in range(samples, measurements-samples):
-        stddev = numpy.std(diff_volume[i-samples:i+samples])
-        mean = numpy.mean(diff_volume[i-samples : i+samples])
+    for i in range(samples, differential_volume_steps-samples):
+        stddev = numpy.std(differential_volume[i-samples:i+samples])
+        mean = numpy.mean(differential_volume[i-samples : i+samples])
         cv[i] = stddev/mean 
     
     return cv
@@ -77,6 +72,8 @@ def generate_plot(droppedFile, show_result = 0):
     T = []
     Xabs = []
 
+    header = lines[0].split(sep= ',')
+
     for line in lines[1:]:
         splitvalues = line.split(sep= ',')
         X.append(float(splitvalues[0]))
@@ -84,47 +81,47 @@ def generate_plot(droppedFile, show_result = 0):
         Z.append(float(splitvalues[2]))
         S.append(float(splitvalues[3]))
         T.append(float(splitvalues[4]))
-        if T[-1] <= 2000:
-            Xabs.append(T[-1]*18 + X[-1])
+        if T[-1] <= blender_frames:                ####There must be a better way
+            Xabs.append(T[-1]*blender_groud_speed*blender_timestep + X[-1])
         else:
             Xabs.append(numpy.mean(X))
             S[-1] = 0
             
-    
+    discrete_time_step = 0.1
+    diff_volume = getVolumeDiferential(T,S, discrete_time_step,blender_timestep, blender_frames)
+    total_volume = integrateVolumeDiferential(diff_volume)
+    cv_samples = 100
+    cvs = calc_cv(diff_volume,cv_samples)
+    cv_min = getMin(cvs)
+
+    ###Generates the plot
     plt.figure()
+    plt.suptitle("parâmetros de teste\n")
+
     plt.subplot(411)
     plotscale = 0.5
     plt.scatter(Xabs,Y,s=[i * plotscale for i in S], edgecolors='none')
     plt.ylim(-300,300)
     plt.title("Distribuição")
-    plt.xlim(0,36000)
-    # if show_result: 
-    #     plt.show()
-    # else:
-    #     plt.savefig(droppedFile[:-4]+"_distribution.png", dpi=1200)
-    # plt.figure()
-    plt.subplot(412)
+    plt.xlim(0, blender_groud_speed*blender_simulation_time)
+    plt.xlabel("[m]")
 
-    diff_volume = timeDistribution(T,S, 0.1)
-
-    volume = [sum(diff_volume[0:i]) for i in range(len(diff_volume))]
-    plt.plot(volume)
+    plt.subplot(412)    
+    plt.plot(numpy.linspace(0,blender_simulation_time,len(total_volume)), total_volume)
     plt.title("Volume dosado")
     plt.ylabel("[cm^3]")
-    plt.xlim(0, 200)
+    plt.xlim(0, blender_simulation_time)
 
     plt.subplot(413)
-    # diff_volume = getDistribution(X,S,10)
-    plt.plot(diff_volume, 'green')
-    plt.xlim(0, 200)
+    plt.plot(numpy.linspace(0,blender_simulation_time,len(diff_volume)),diff_volume, 'green')
+    plt.xlim(0, blender_simulation_time)
     plt.title("Variação volumétrica")
-    plt.subplot(414)
-    cvs = calc_cv(diff_volume,100)
+    
+    plt.subplot(414)    
     plt.plot(cvs, 'red')
-    plt.title("CV")
-    plt.xlim(0, 200)
+    plt.title("CV para %s amostras" %(cv_samples))
+    plt.xlim(0, len(cvs))
 
-    cv_min = getMin(cvs)
     plt.text(0.1,cv_min+0.1, "Min cv: %.3f" %(cv_min))
     plt.hlines(cv_min, 0, len(cvs), linestyles= 'dashed')
     plt.tight_layout(pad=0.5)
@@ -134,7 +131,6 @@ def generate_plot(droppedFile, show_result = 0):
         plt.savefig(droppedFile[:-4]+"_analysis.png", dpi=1200)
     
 
-   
 
 if len(sys.argv) > 1:
     for i in range(1, len(sys.argv)):
